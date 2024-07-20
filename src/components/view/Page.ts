7265306e5d3241be723e729d152976fccbaafa5b
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2024-07-20 15:16:25 +0500

    feat: разработать классы страницы и модального окна успешного заказа

--- src/components/view/Page.ts
+++ src/components/view/Page.ts
@@ -1,4 +1,6 @@
+import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
+import { IEvents } from "../base/events";
 
 // тип описания страницы
 interface IPage {
@@ -16,4 +18,36 @@ export class Page extends Component<IPage> {
     protected _catalog: HTMLElement;
     protected _wrapper: HTMLElement;
     protected _basket: HTMLElement;
+
+    constructor(container: HTMLElement, protected events: IEvents) {
+        super(container);
+
+        this._counter = ensureElement<HTMLElement>('.header__basket-counter');
+        this._catalog = ensureElement<HTMLElement>('.gallery');
+        this._wrapper = ensureElement<HTMLElement>('.page__wrapper');
+        this._basket = ensureElement<HTMLElement>('.header__basket');
+
+        this._basket.addEventListener('click', () => {
+            this.events.emit('basket:open');
+        })
+    }
+
+    // сеттер для счетчика товаров в корзине
+    set counter(value: number) {
+        this.setText(this._counter, String(value));
+    }
+
+    // сеттер для карточек товаров на странице
+    set catalog(items: HTMLElement[]) {
+        this._catalog.replaceChildren(...items);
+    }
+
+    // сеттер для блока прокрутки
+    set locked(value: boolean) {
+        if (value) {
+            this._wrapper.classList.add('.page__wrapper_locked');
+        } else {
+            this._wrapper.classList.remove('.page__wrapper_locked');
+        }
+    }
 }
--- src/components/view/Success.ts
+++ src/components/view/Success.ts
@@ -1,12 +1,32 @@
+import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
 
 // интерфейс описания модального окна успешного заказа
 interface ISuccess {
-    description: string;
+    description: number;
+}
+
+interface ISuccessAction {
+    onClick: (event: MouseEvent) => void;
 }
 
 // Класс создания модального окна успешного заказа
 export class Success extends Component<ISuccess> {
     protected _description: HTMLElement;
     protected _button: HTMLButtonElement;
+
+    constructor(container: HTMLElement, actions?: ISuccessAction) {
+        super(container);
+
+        this._description = ensureElement<HTMLElement>('film__description');
+        this._button = ensureElement<HTMLButtonElement>('.modal__close');
+
+        if (actions?.onClick) {
+            this._button.addEventListener('click', () => actions.onClick);
+        }
+    }
+
+    set description(value: number) {
+        this._description.textContent = 'Списано' + '' + value + '' + 'синапсов';
+    }
 }
